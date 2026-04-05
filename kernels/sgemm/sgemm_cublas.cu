0b0a4f58f61c555999f4c755d93b1f372fc66ff9
lnxtree <113999814+lnxtree@users.noreply.github.com>
2026-04-05 22:14:26 +0800

    fix(sgemm): destroy cublas handle to avoid alloc failed (#415)

--- kernels/sgemm/sgemm_cublas.cu
+++ kernels/sgemm/sgemm_cublas.cu
@@ -14,31 +14,43 @@
 
 #include "cublas_v2.h"
 
+#define CHECK_CUBLAS(call)                                                     \
+  do {                                                                         \
+    cublasStatus_t status = (call);                                            \
+    if (status != CUBLAS_STATUS_SUCCESS) {                                     \
+      throw std::runtime_error("cuBLAS call failed");                         \
+    }                                                                          \
+  } while (0)
+
 void cublas_sgemm(float *A, float *B, float *C, size_t M, size_t N, size_t K) {
   cublasHandle_t handle = nullptr;
-  cublasCreate(&handle);
-  cublasSetMathMode(handle, CUBLAS_DEFAULT_MATH);
+  CHECK_CUBLAS(cublasCreate(&handle));
+  CHECK_CUBLAS(cublasSetMathMode(handle, CUBLAS_DEFAULT_MATH));
 
   static float alpha = 1.0;
   static float beta = 0.0;
 
-  cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha, B, CUDA_R_32F,
-               N, A, CUDA_R_32F, K, &beta, C, CUDA_R_32F, N, CUBLAS_COMPUTE_32F,
-               CUBLAS_GEMM_DEFAULT);
+  CHECK_CUBLAS(cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha,
+                            B, CUDA_R_32F, N, A, CUDA_R_32F, K, &beta, C,
+                            CUDA_R_32F, N, CUBLAS_COMPUTE_32F,
+                            CUBLAS_GEMM_DEFAULT));
+  CHECK_CUBLAS(cublasDestroy(handle));
 }
 
 void cublas_sgemm_tf32(float *A, float *B, float *C, size_t M, size_t N,
                        size_t K) {
   cublasHandle_t handle = nullptr;
-  cublasCreate(&handle);
-  cublasSetMathMode(handle, CUBLAS_TF32_TENSOR_OP_MATH);
+  CHECK_CUBLAS(cublasCreate(&handle));
+  CHECK_CUBLAS(cublasSetMathMode(handle, CUBLAS_TF32_TENSOR_OP_MATH));
 
   static float alpha = 1.0;
   static float beta = 0.0;
 
-  cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha, B, CUDA_R_32F,
-               N, A, CUDA_R_32F, K, &beta, C, CUDA_R_32F, N, CUBLAS_COMPUTE_32F,
-               CUBLAS_GEMM_DEFAULT_TENSOR_OP);
+  CHECK_CUBLAS(cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha,
+                            B, CUDA_R_32F, N, A, CUDA_R_32F, K, &beta, C,
+                            CUDA_R_32F, N, CUBLAS_COMPUTE_32F,
+                            CUBLAS_GEMM_DEFAULT_TENSOR_OP));
+  CHECK_CUBLAS(cublasDestroy(handle));
 }
 
 #define STRINGFY(str) #str
